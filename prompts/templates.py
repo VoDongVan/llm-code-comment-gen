ef5df4a2401cfd0cc78da8cfbeee3b8fd8010eda
Van Dong Vo <dongvan18032003@gmail.com>
2025-04-10 14:29:43 -0400

    move prev_context, next_context into code parameter

--- prompts/templates.py
+++ prompts/templates.py
@@ -1,41 +1,49 @@
-def zero_shot_prompt(code: str, type: str, prev_context=None, next_context=None):
-    if type == "inline":
-        prompt = f"""This is the previous context of a line:\n{prev_context}\n\n.\
-            This is the next context of a line:\n{next_context}\n\n.\
+def format_inline_prompt(prev_context, next_context):
+    return f"""This is the previous context of a line:\n{prev_context}\n\n\
+            This is the next context of a line:\n{next_context}\n\n\
             Generate helpful comments that should be put in the line"""
+
+def format_code_prompt(code):
+    return f"""Generate helpful comments for the following code snippet: {code}"""
+
+def zero_shot_prompt(input: object, type: str):
+    if type == "inline":
+        prev_context = input["prev_context"]
+        next_context = input["next_context"]
+        prompt = format_inline_prompt(prev_context, next_context)
     else:
-        prompt = f"""Generate helpful comments for the following code snippet: {code}"""
+        code = input["code"]
+        prompt = format_code_prompt(code)
     prompt = {"role": "user", "content": prompt}
     return [prompt]
 
-def few_shot_prompt(code: str, few_shot_examples: list, type: str, prev_context=None, next_context=None):
+def few_shot_prompt(input: object, few_shot_examples: list, type: str):
     prompt = f""""""
     if type == "inline":
+        prev_context = input["prev_context"]
+        next_context = input["next_context"]
         for example in few_shot_examples:
             prev = example["prev_context"]
             next = example["next_context"]
             comment = example["comment"]
-            prompt += f"Previous context:\n {prev}\n\n"
-            prompt += f"Next context:\n {next}\n\n"
-            prompt += f"Comments:\n {comment}"
-        prompt = f"""Here are examples of comments for code snippet:\n{prompt}.\n\n\
-            This is the previous context of a line:\n{prev_context}\n\n.\
-            This is the next context of a line:\n{next_context}\n\n.\
-            Generate helpful comments that should be put in the line"""
+            prompt += f"Example:\nPrevious context:\n{prev}\n\nNext context:\n{next}\n\nComment:\n{comment}\n\n---\n\n"
+        prompt = f"""Here are examples of comments for code snippet:\n{prompt}\n\n\
+            """ + format_inline_prompt(prev_context, next_context)
     else:
+        code = input["code"]
         for example in few_shot_examples:
-            code = example["code"]
+            code_fs = example["code"]
             comment = example["comment"]
-            prompt += f"Function:\n {code}\n\n"
-            prompt += f"Comment:\n {comment}\n\n"
-        prompt = f"""Here are examples of comments for code snippet:\n{prompt}.\n\n\
-            Generate helpful comments for the following code snippet: {code}"""
+            prompt += f"Example:\nFunction:\n{code_fs}\n\nComment:\n{comment}\n\n---\n\n"
+        prompt = f"""Here are examples of comments for code snippet:\n{prompt}\n\n\
+            """ + format_code_prompt(code)
     prompt = {"role": "user", "content": prompt}
     return [prompt]
 
-def cot_prompt(code: str, type: str, prev_context=None, next_context=None):
+def cot_prompt(input: object, type: str):
     if type == "class":
-        prompt1 = f"""Analyze the following code snippet:\n{code}\n\n.
+        code = input["code"]
+        prompt1 = f"""Analyze the following code snippet:\n{code}\n\n
             Answer these questions:\n\
             1. What are class variables?
             2. What are the purposes of these variables?
@@ -45,7 +53,8 @@ def cot_prompt(code: str, type: str, prev_context=None, next_context=None):
             6. What are the functionalities of these private methods?
             """
     elif type == "function":
-        prompt1 = f"""Analyze the following code snippet:\n{code}\n\n.\
+        code = input["code"]
+        prompt1 = f"""Analyze the following code snippet:\n{code}\n\n\
             Answer these questions:\n\
             1. What are the inputs?
             2. What are the outputs?
@@ -54,11 +63,13 @@ def cot_prompt(code: str, type: str, prev_context=None, next_context=None):
             5. Are there any constraints or specific conditions that a user should be aware of?
             """
     else:
-        prompt1 = f"""This is the previous context of a line:\n{prev_context}\n\n.\
-            This is the next context of a line:\n{next_context}\n\n.\
+        prev_context = input["prev_context"]
+        next_context = input["next_context"]
+        prompt1 = f"""This is the previous context of a line:\n{prev_context}\n\n\
+            This is the next context of a line:\n{next_context}\n\n\
             Answer these questions:\n\
             1. What are the variables and functions used in previous context?
-            2. What does the previouse context do?
+            2. What does the previous context do?
             3. What are the variables and functions used in next context?
             4. What does the next context do?
             5. Should comments be written here, between these 2 contexts?
@@ -68,13 +79,14 @@ def cot_prompt(code: str, type: str, prev_context=None, next_context=None):
     prompt2 = {"role": "user", "content": prompt2}
     return [prompt1, prompt2]
 
-def critique_prompt(code: str, type: str, prev_context=None, next_context=None):
+def critique_prompt(input: object, type: str):
     if type == "inline":
-        prompt1 = f"""This is the previous context of a line:\n{prev_context}\n\n.\
-            This is the next context of a line:\n{next_context}\n\n.\
-            Generate helpful comments to be put in the line"""
+        prev_context= input["prev_context"]
+        next_context = input["next_context"]
+        prompt1 = format_inline_prompt(prev_context, next_context)
     else:
-        prompt1 = f"""Generate helpful comments for the following code snippet: {code}"""
+        code = input["code"]
+        prompt1 = format_code_prompt(code)
     prompt2 = f"""Review and find problems with your answer"""
     prompt3 = f"""Based on the problems, improve your answer"""
     prompt1 = {"role": "user", "content": prompt1}
@@ -82,29 +94,30 @@ def critique_prompt(code: str, type: str, prev_context=None, next_context=None):
     prompt3 = {"role": "user", "content": prompt3}
     return [prompt1, prompt2, prompt3]
 
-def expert_prompt(code: str, type: str, prev_context=None, next_context=None):
+def expert_prompt(input: object, type: str):
     prompt1 = f"""Describe an experienced software engineer that can write high-quality code comments in second person perspective:\n"""
     if type == "inline":
-        prompt2 = f"""This is the previous context of a line:\n{prev_context}\n\n.\
-            This is the next context of a line:\n{next_context}\n\n.\
-            Generate helpful comments that should be put in the line"""
+        prev_context = input["prev_context"]
+        next_context = input["next_context"]
+        prompt2 = format_inline_prompt(prev_context, next_context)
     else:
-        prompt2 = f"""Generate helpful comments for the following code snippet: {code}"""
+        code = input["code"]
+        prompt2 = format_code_prompt(code)
     prompt1 = {"role": "user", "content": prompt1}
     prompt2 = {"role": "user", "content": prompt2}
     return [prompt1, prompt2]
 
-def prepare_prompt(code: str, data_type: str, few_shot_examples=None, prev_context=None, next_context=None, prompt_type="zero-shot"):
+def prepare_prompt(input: object, data_type: str, few_shot_examples=None, prompt_type="zero-shot"):
     if prompt_type == "zero-shot":
-        prompts = zero_shot_prompt(code, data_type, prev_context, next_context)
+        prompts = zero_shot_prompt(input, data_type)
     elif prompt_type == "few-shot":
-        prompts = few_shot_prompt(code, data_type, few_shot_examples, prev_context, next_context)
+        prompts = few_shot_prompt(input, data_type, few_shot_examples)
     elif prompt_type == "cot":
-        prompts = cot_prompt(code, data_type, prev_context, next_context)
+        prompts = cot_prompt(input, data_type)
     elif prompt_type == "critique":
-        prompts = critique_prompt(code, data_type, prev_context, next_context)
+        prompts = critique_prompt(input, data_type)
     elif prompt_type == "expert_prompt":
-        prompts = expert_prompt(code, data_type, prev_context, next_context)
+        prompts = expert_prompt(input, data_type)
     else:
         raise ValueError("Prompt strategy not supported")
     return prompts
